1c563d7b88d80ec2c9627a5a98eff701fc88adf4
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-01 21:39:18 -0500

    Include index.html in purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
   purge: {
-    content: ["./src/**/*.svelte"],
+    content: ["./public/index.html", "./src/**/*.svelte"],
     enabled: production,
   },
   darkMode: false, // or 'media' or 'class'
