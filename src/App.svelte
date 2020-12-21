4ce48ab9ac2204dd09241312f53a61190e5c6222
Dakota Lillie <dakota.lillie@icloud.com>
2020-12-20 23:16:46 -0500

    Add page background color

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,18 @@
 <script>
+  import Global from "./Global.svelte";
   import Header from "./Header.svelte";
   import Main from "./Main.svelte";
   import Footer from "./Footer.svelte";
 </script>
 
-<style global lang="postcss">
-  @tailwind base;
-  @tailwind components;
-  @tailwind utilities;
-
-  html {
+<style>
+  :global(html) {
+    @apply bg-gray-900;
     scroll-behavior: smooth;
   }
 </style>
 
+<Global />
 <Header />
 <Main />
 <Footer />
